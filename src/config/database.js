c197f10e2e80573843707a32d7b979ddfab420b3
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2024-04-25 23:57:28 +0900

    fix: socket 경로 설정 추가

--- src/config/database.js
+++ src/config/database.js
@@ -5,18 +5,24 @@ const database = process.env.DB_DATABASE;
 const host = process.env.DB_HOST;
 const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
+const port = process.env.DB_PORT || 3306;
 
 const sequelize = new Sequelize(database, user, password, {
     host: host,
     dialect: 'mysql',
+    port: port,
+    dialectOptions: {
+        socketPath: '/tmp/mysql.sock'
+      }
 });
+
 
 sequelize.authenticate()
 .then(() => {
-console.log('Connection has been established successfully.');
+    console.log('데이터베이스 연결 성공');
 })
 .catch(err => {
-console.error('Unable to connect to the database:', err);
+    console.error('데이터베이스 연결 오류 :', err);
 });
 
   
