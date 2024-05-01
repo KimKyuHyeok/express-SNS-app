92e0b844479817b59e1692c43f6a57ca86f297ee
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2024-05-01 21:51:01 +0900

    fix: 댓글 기능으로 인해 경로 수정

--- src/server.js
+++ src/server.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
 app.use('/', mainRouter);
 app.use('/auth', userRouter);
 app.use('/posts', postsRouter);
-app.use('/posts/:id/comments', commentsRouter);
+app.use('/posts/', commentsRouter);
 app.use('/profile/:id', profileRouter);
 app.use('/friends', friendsRouter);
 app.use('/posts/:id/like', likesRouter);
