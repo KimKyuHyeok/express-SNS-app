a2846389b9c18bb585db02644db65e0faee52387
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2024-05-06 22:52:31 +0900

    feat: profile 수정 기능 구현

--- src/api/routes/profile.router.js
+++ src/api/routes/profile.router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { isAuth } = require('../middleware/auth');
+const { isAuth, checkIsMe } = require('../middleware/auth');
 const post = require('../../model/posts.model');
 const User = require('../../model/users.model');
 const comments = require('../../model/comments.model');
@@ -37,6 +37,37 @@ router.get('/:id', isAuth, async (req, res) => {
         req.flash('error', '게시물을 가져오는데 실패했습니다.')
         res.redirect('back');
     });
+})
+
+router.get('/:id/edit', checkIsMe, (req, res) => {
+    res.render('profile/edit', {
+        user: req.user
+    });
+})
+
+router.put('/:id', checkIsMe, (req, res) => {
+    User.findOne({ where : { id: req.params.id }})
+    .then((user) => {
+        user.firstName = req.body.firstName,
+        user.lastName = req.body.lastName,
+        user.bio = req.body.bio,
+        user.hometown = req.body.hometown,
+        user.workplace = req.body.workplace,
+        user.education = req.body.education,
+        user.contact = req.body.contact
+        
+        try {
+            user.save();
+            req.flash('success', '회원정보가 성공적으로 변경되었습니다.')
+            res.redirect('/profile/' + req.user.id);
+        } catch (err) {
+            req.flash('error', '회원정보가 수정 도중에 실패하였습니다.')
+            res.redirect('/profile/' + req.user.id);
+        }
+    }).catch((err) => {
+        req.flash('error', '회원정보 수정 실패');
+        res.redirect('back');
+    })
 })
 
 module.exports = router
